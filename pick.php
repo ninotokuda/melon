d8d701ed55fde2c3286f28f70d28cf3df6891476
Nino Tokuda <ntstudio.nino@gmail.com>
2014-02-16 15:04:38 -0800

    changed html to php

---
+++ pick.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>MELON+ - START</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Fonts -->
+    <link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+
+    <!-- Custom Theme CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/grayscale.css" rel="stylesheet">
+    <link href="css/color.css" rel="stylesheet">
+    <link href="css/landing.css" rel="stylesheet">
+    <link href="css/fancy.css" rel="stylesheet">
+  <!--  <link href="css/portfolio-item.css" rel="stylesheet">
+ <!--   <link href="css/portfolio_styling.css" rel="stylesheet">   -->
+
+
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-custom">
+
+    <nav class="navbar navbar-custom navbar-fixed-top bg-col-torkiz  opacity" role="navigation">
+        <div class="container no-opacity">
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="index.html">
+                    <i class="glyphicon glyphicon-heart bg-col-redpink opacity"></i>  <span class="light">MELON</span>
+                </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                <ul class="nav navbar-nav">
+                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#about">About</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#download">START</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+
+
+    <section class="pick">
+        <div class="intro-body">
+            <div class="container">
+                <div class="row">
+                    <ul class="col-sm-12">
+                        <li class="melon-container col-sm-6">
+                            <h3 class="brand-heading">MELON</h3>
+                        </li>
+                        <li class="melon-container col-sm-6">
+                            <h3 class="brand-heading">MELON</h3>
+                        </li>
+                      </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- Core JavaScript Files -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="js/jquery-1.10.2.js"></script>
+    <script src="js/knob.js"></script>
+    <script src="js/jquery.roundabout.js"></script>
+    <script src="js/jquery.roundabout-shapes.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/itemPick.js"></script>
+
+    <!-- Google Maps API Key - You will need to use your own API key to use the map feature -->
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA&sensor=false"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/grayscale.js"></script>
+
+</body>
+
+</html>
